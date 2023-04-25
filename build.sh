ee4b364c1fa49ea947fde469b92d67c1b710b021
Veni92 <116999531+Veni92@users.noreply.github.com>
2023-04-25 17:40:01 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash
+sudo docker stop $(docker ps -a -q)
+sudo docker rm $(docker ps -a -q)
+sudo docker image rmi -f veni9297/prod:react-app
+sudo docker image rmi -f veni9297/dev:react-app
+sudo docker rmi -f react-app:latest
+sudo  docker rmi -f node:latest
+sudo docker build -t react-app:latest .
